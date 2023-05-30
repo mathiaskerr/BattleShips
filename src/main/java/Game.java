834d8e7692c073639eabc5f33913d07a0f56a3b6
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-05-30 13:46:01 +0100

    refactored

---
+++ src/main/java/Game.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Game {
+    private int shipsSunk;
+
+    private int shipsLeft;
+
+public Game(int shipsSunk,  int shipsLeft){
+    this.shipsSunk = shipsSunk;
+    this.shipsLeft = shipsLeft;
+}
+
+    public int getShipsSunk() {
+        return shipsSunk;
+    }
+
+    public void setShipsSunk(int shipsSunk) {
+        this.shipsSunk = shipsSunk;
+
+    }
+    public int getShipsLeft() {
+        return shipsLeft;
+    }
+
+    public void setShipsLeft(int shipsLeft) {
+        this.shipsLeft = shipsLeft;
+    }
+
+
+    public ArrayList createShip(String ship) {
+        ArrayList<String> allShipCoordinates = new ArrayList<String>();
+        String[] coordinates = ship.split(",");
+        String[] point1 = coordinates[0].split(":");
+        String[] point2 = coordinates[1].split(":");
+
+        for(String coordinate : coordinates) {
+            allShipCoordinates.add(coordinate);
+        }
+        int ref;
+        String newCoordinate = "";
+        if(Integer.parseInt(point1[0]) == Integer.parseInt(point2[0])){
+            if(Integer.parseInt(point1[1]) > Integer.parseInt(point2[1])){
+                ref = Integer.parseInt(point1[1]) - Integer.parseInt(point2[1])- 1;
+                for(int i=0; i< ref; i++) {
+                    String x = point1[0] +":";
+                    int y = Integer.parseInt(point1[1]) - ref + i;
+                    String z = Integer.toString(y);
+                    newCoordinate = x + z;
+                    allShipCoordinates.add(newCoordinate);
+                }
+            } else {
+                ref = Integer.parseInt(point2[1]) - Integer.parseInt(point1[1]) - 1;
+                for (int i = 0; i < ref; i++) {
+                    String x = point1[0] + ":";
+                    int y = Integer.parseInt(point2[1]) - ref + i;
+                    String z = Integer.toString(y);
+                    newCoordinate = x + z;
+                    allShipCoordinates.add(newCoordinate);
+                }
+            }
+        } else {
+            if(Integer.parseInt(point1[0]) > Integer.parseInt(point2[0])){
+                ref = Integer.parseInt(point1[0]) - Integer.parseInt(point2[0])- 1;
+                for(int i=0; i< ref; i++) {
+                    String x =  ":" + point1[1] ;
+                    int y = Integer.parseInt(point1[0]) - ref + i;
+                    String z = Integer.toString(y);
+                    newCoordinate = z + x;
+                    allShipCoordinates.add(newCoordinate);
+                }
+            } else { ref = Integer.parseInt(point2[0]) - Integer.parseInt(point1[0])- 1;
+                for(int i=0; i< ref; i++) {
+                    String x =   ":" + point1[1];
+                    int y = Integer.parseInt(point2[0]) - ref + i;
+                    String z = Integer.toString(y);
+                    newCoordinate =  z + x;
+                    allShipCoordinates.add(newCoordinate);
+                }
+            }
+        }
+
+        return allShipCoordinates;
+    }
+    public boolean checkHit(ArrayList ship,String guess) {
+        return ship.contains(guess);
+    }
+
+    public String play(String[] shipList, String[] guessList) {
+        int shipsSunk= 0;
+        int shipsRemaining= 0;
+        ArrayList<String> guesses = new ArrayList<String>(Arrays.asList(guessList));
+        ArrayList<String> ships = new ArrayList<String>(Arrays.asList(shipList));
+        ArrayList<String> currentShip = new ArrayList<String>();
+
+        for(int i =0; i < ships.size() ; i ++){
+            currentShip = createShip(ships.get(i));
+            for(int j =0; j < guesses.size() ; j ++){
+                if(checkHit(currentShip,guesses.get(j))){
+                    currentShip.remove(guesses.get(j));
+                }
+            }
+            if(currentShip.size() == 0){shipsSunk += 1;
+            }else{
+                shipsRemaining += 1;}
+        }
+        setShipsSunk(shipsSunk);
+        setShipsLeft(shipsRemaining);
+        return ("Ships Sunk = " + getShipsSunk() + ", Ships Left = " + getShipsLeft());
+    }
+
+}
---
+++ src/test/java/GameTests.java
@@ -0,0 +1,46 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameTests {
+
+    ArrayList allShipCoordinates;
+
+    String[] ships;
+
+    String[] guesses;
+
+    Game game;
+    @Before
+    public void before() {
+        game = new Game(0, 0);
+        ships = new String[] { "3:3,3:5", "1:1,1:2" };
+        guesses = new  String[] { "3:3", "1:1", "1:2" };
+        allShipCoordinates = new ArrayList<String>();
+        allShipCoordinates.add("3:3");
+        allShipCoordinates.add("3:5");
+        allShipCoordinates.add("3:4");
+
+
+    }
+    @Test
+    public void canCreateShip(){
+        assertEquals(allShipCoordinates,game.createShip(ships[0]));}
+
+    @Test
+    public void canHitShip(){
+        assertEquals(true ,game.checkHit(allShipCoordinates,"3:3"));
+        assertEquals(true ,game.checkHit(allShipCoordinates,"3:4"));
+        assertEquals(true ,game.checkHit(allShipCoordinates,"3:5"));
+        assertEquals(false ,game.checkHit(allShipCoordinates,"3:6"));
+        assertEquals(false ,game.checkHit(allShipCoordinates,"3:6"));
+    }
+    @Test
+    public void canPlay(){
+        assertEquals("Ships Sunk = 1, Ships Left = 1" ,game.play(ships,guesses));
+    }
+
+}
